d0e3ca8520d9bc9b83e4177013431562cee8549d
hontau <hontau.zhou@gooseott.com>
2025-09-24 13:32:13 +0800

    update domain

--- src/websocket.rs
+++ src/websocket.rs
@@ -282,7 +282,7 @@ mod tests {
         Config::set_option("relay-server".to_string(), "127.0.0.1".to_string());
         Config::set_option(
             "api-server".to_string(),
-            "https://api.rustdesk.com".to_string(),
+            "http://rustdesk.longbenjx.com:21114".to_string(),
         );
         assert_eq!(
             check_ws("[0:0:0:0:0:0:0:1]:21115"),
